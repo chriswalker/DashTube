d4cd731ec2a865b128e6e4eb4d173178363115b9
Chris Walker <thatamazingweb@gmail.com>
2014-01-18 22:10:26 +0000

    Change ID to String (easier for comparisons)

--- DashTube/src/main/java/com/taw/dashtube/model/Line.java
+++ DashTube/src/main/java/com/taw/dashtube/model/Line.java
@@ -24,7 +24,7 @@ import com.google.api.client.xml.GenericXml;
  */
 public class Line extends GenericXml {
     @Key("@ID")
-    public int id;
+    public String id;
     @Key("@Name")
     public String name;
 }
